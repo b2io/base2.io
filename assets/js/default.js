55280db59bc8d7eb405f47555a08783d946185ea
Drew Miller <dmiller@base2.io>
2013-10-18 11:00:00 -0400

    Add GA event tracking to lead form.

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,4 +1,4 @@
-(function ($) {
+(function ($, GoogleAnalytics) {
 
   // Set defaults for jQuery Validator to work with Bootstrap 3.
   $.validator.setDefaults({
@@ -51,6 +51,11 @@
         'lead[description]': 'Please enter a description.'
       },
 
+      invalidHandler: function () {
+        // Track the attempted submission.
+        GoogleAnalytics.push(['_trackEvent', 'Lead', 'Send', 'Invalid']);
+      },
+
       submitHandler: function () {
         // Pull the values out of the UI.
         var name = $('[name="lead[name]"]').val(),
@@ -60,6 +65,9 @@
           description = $('[name="lead[description]"]').val(),
           mailtoUrl;
 
+        // Track the successful submission.
+        GoogleAnalytics.push(['_trackEvent', 'Lead', 'Send', 'Valid']);
+
         // Convert the fields to a `mailto` link and open it:
         mailtoUrl = 'mailto:info@base2.io?subject=Let\'s%20Work%20Together!&body=' + encodeURIComponent('Name: ' + name + '\n' + 'Email: ' + email + '\n' + 'Phone: ' + phoneNumber + '\n' + 'Budget: ' + budget + '\n' + 'Description: ' + description);
         window.open(mailtoUrl, '_blank');
@@ -68,4 +76,4 @@
 
   });
 
-})(jQuery);
+})(jQuery, _gaq);
