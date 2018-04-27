f99b18329007db17ca63698124a4a1f08cedde7d
Caitlin Steinert <caitlinsteinert@gmail.com>
2018-04-27 15:31:08 -0400

    Reverted to JPGs, responsive-ified the layout.

--- src/components/molecules/TeamList.jsx
+++ src/components/molecules/TeamList.jsx
@@ -2,70 +2,144 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { P, Img, UL, LI } from '../atoms';
-import { themed } from '../../util/style';
+import { mediaQuery, themed } from '../../util/style';
 
-const imageSize = '150px';
+const imageSize = {
+  default: '70px',
+  medium: '110px',
+  large: '150px',
+};
 
 const TeamList = styled(UL)`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
   list-style: none;
   margin: 3em 0;
   padding: 0;
-  text-align: center;
+  width: 100%;
+
+  ${mediaQuery.small`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    text-align: center;
+  `};
 `;
 
 const Wrapper = styled(LI)`
   align-items: center;
   display: flex;
-  flex-direction: column;
-  margin: 0 2em 3em;
-  text-align: center;
+  margin: 0 1% 3em;
+  position: relative;
   vertical-align: top;
-  width: calc(${imageSize} * 2);
+
+  ${mediaQuery.small`
+    flex-direction: column;
+    margin: 0 4% 3em;
+    text-align: center;
+    width: 24%;
+  `};
+
+  ${mediaQuery.medium`
+    margin: 0 5% 3em;
+    width: 20%;
+  `};
+
+  &::after {
+    border: 1px solid ${themed('color.overlay')};
+    border-radius: 50%;
+    content: '';
+    height: calc(${imageSize.default} / 3);
+    position: absolute;
+    top: calc(${imageSize.default} / 2.75);
+    width: calc(${imageSize.default} + 19%);
+    z-index: 0;
+
+    ${mediaQuery.small`
+      width: 100%;
+    `};
+
+    ${mediaQuery.medium`
+      height: calc(${imageSize.medium} / 3);
+      top: calc(${imageSize.medium} / 2.75);
+    `};
+
+    ${mediaQuery.large`
+      height: calc(${imageSize.large} / 3);
+      top: calc(${imageSize.large} / 2.75);
+    `};
+  }
 
   > div {
     position: relative;
-    width: ${imageSize};
+    margin: 0 10% 0 10%;
 
-    &::before,
-    &::after {
-      content: '';
-      display: block;
-      pointer-events: none;
-      position: absolute;
-      overflow: visible;
-    }
+    ${mediaQuery.small`
+      margin: 0;
+      width: ${imageSize.default};
+    `};
+
+    ${mediaQuery.medium`
+      width: ${imageSize.medium};
+    `};
+
+    ${mediaQuery.large`
+      width: ${imageSize.large};
+    `};
 
     &::before {
       border: 2px solid ${themed('color.accent')};
       border-radius: 50%;
       box-shadow: 0 0 0 1px ${themed('color.border')};
-      height: calc(${imageSize} - 3px);
+      content: '';
+      display: block;
+      height: calc(${imageSize.default} - 3px);
+      pointer-events: none;
+      position: absolute;
+      overflow: visible;
       top: 0;
-      width: calc(${imageSize} - 3px);
+      width: calc(${imageSize.default} - 3px);
       z-index: 2;
-    }
 
-    &::after {
-      border: 1px solid ${themed('color.overlay')};
-      border-radius: 50%;
-      height: 3em;
-      left: -50%;
-      top: 35%;
-      width: 200%;
-      z-index: 0;
+      ${mediaQuery.medium`
+        height: calc(${imageSize.medium} - 3px);
+        width: calc(${imageSize.medium} - 3px);
+      `};
+
+      ${mediaQuery.large`
+        height: calc(${imageSize.large} - 3px);
+        width: calc(${imageSize.large} - 3px);
+      `};
     }
   }
 `;
 
 const TeamImg = styled(Img)`
+  border-radius: 50%;
   height: auto;
   position: relative;
   vertical-align: middle;
-  width: ${imageSize};
+  width: ${imageSize.default};
   z-index: 1;
+
+  ${mediaQuery.medium`
+    width: ${imageSize.medium};
+  `};
+
+  ${mediaQuery.large`
+    width: ${imageSize.large};
+  `};
+`;
+
+const TeamMemberInfo = styled(P)`
+  margin-left: 5%;
+  max-width: 50%;
+
+  ${mediaQuery.small`
+    margin-left: 0;
+    margin-top: .5em;
+    max-width: 100%;
+    text-align: center;
+    width: 100%;
+  `};
 `;
 
 const Title = styled.span`
@@ -76,10 +150,10 @@ function TeamListItem({ image, name, title }) {
   return (
     <Wrapper>
       <TeamImg {...image} alt={name} title={name} />
-      <P>
+      <TeamMemberInfo>
         {name}
         <Title>{title}</Title>
-      </P>
+      </TeamMemberInfo>
     </Wrapper>
   );
 }
