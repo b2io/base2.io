48413a8179d0edbedcdc7e6c19dd03a5a6b5ccd3
Caitlin Steinert <caitlinsteinert@gmail.com>
2018-10-03 11:14:18 -0400

    Updates color and size of jobs logo

--- src/components/atoms/logos/BaseTwoJobsLogo.jsx
+++ src/components/atoms/logos/BaseTwoJobsLogo.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 import BaseTwoJobsLogoSVG from './b2-jobs-logo.svg';
+import { themed } from '../../../util/style';
+
+const StyledLogo = styled(BaseTwoJobsLogoSVG)`
+  fill: ${themed('color.white')};
+`;
 
 function BaseTwoJobsLogo() {
-  return <BaseTwoJobsLogoSVG />;
+  return <StyledLogo />;
 }
 
 export default BaseTwoJobsLogo;
