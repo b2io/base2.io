049104b26ec434fa20cb1de64ca89c7cbc5bf756
Reed Dunkle <reeddunkle@gmail.com>
2018-10-03 11:15:57 -0400

    WIP: label click not propogating

--- src/components/atoms/form/RadioButton.jsx
+++ src/components/atoms/form/RadioButton.jsx
@@ -1,6 +1,17 @@
-import styled from 'styled-components';
+import React from 'react';
+import nanoid from 'nanoid';
+import FormField from './FormField';
+import Label from './Label';
+// import styled from 'styled-components';
 // import { cssSome, themed } from '../../../util/style';
 
-const RadioButton = styled.input;
+function RadioButton({ id = nanoid(), label, ...rest }) {
+  return (
+    <FormField>
+      <input {...rest} id={id} type="radio" />
+      <Label htmlFor={id}>{label}</Label>
+    </FormField>
+  );
+}
 
 export default RadioButton;
