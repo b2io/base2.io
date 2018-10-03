0a4b32bbdd2ef7b5f33527950054de288b1c8eea
Sean Ludemann <sean.ludemann@gmail.com>
2018-10-03 10:40:20 -0400

    wip

---
+++ src/components/molecules/JobsHeader.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Img } from '../atoms';
+import { mediaQuery } from '../../util/style';
+
+const imageSize = {
+  default: '70px',
+  medium: '110px',
+  large: '150px',
+};
+
+const AstronautImg = styled(Img)`
+  border-radius: 50%;
+  height: auto;
+  position: relative;
+  vertical-align: middle;
+  width: ${imageSize.default};
+  z-index: 1;
+
+  ${mediaQuery.medium`
+    width: ${imageSize.medium};
+  `};
+
+  ${mediaQuery.large`
+    width: ${imageSize.large};
+  `};
+`;
+
+const JoinOurTeamImg = styled(Img)`
+  border-radius: 50%;
+  height: auto;
+  position: relative;
+  vertical-align: middle;
+  width: ${imageSize.default};
+  z-index: 1;
+
+  ${mediaQuery.medium`
+    width: ${imageSize.medium};
+  `};
+
+  ${mediaQuery.large`
+    width: ${imageSize.large};
+  `};
+`;
+
+function JobsHeader() {
+  return (
+    <div>
+      <AstronautImg src="../../../_content/team/chawkins.jpg" />
+      <JoinOurTeamImg src="../../../_content/team/chawkins.jpg" />
+    </div>
+  );
+}
+
+export default JobsHeader;
