e21dfaa1d198a8cc493e4d7e4a5baa546e6b3818
Reed Dunkle <reeddunkle@gmail.com>
2018-10-03 11:31:14 -0400

    WIP: click events working

--- src/components/atoms/form/RadioButton.jsx
+++ src/components/atoms/form/RadioButton.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import nanoid from 'nanoid';
 import FormField from './FormField';
-import Label from './Label';
 // import styled from 'styled-components';
 // import { cssSome, themed } from '../../../util/style';
 
 function RadioButton({ id = nanoid(), label, ...rest }) {
   return (
     <FormField>
-      <input {...rest} id={id} type="radio" />
-      <Label htmlFor={id}>{label}</Label>
+      <label htmlFor={id}>
+        <input {...rest} id={id} type="radio" />
+        {label}
+      </label>
     </FormField>
   );
 }
