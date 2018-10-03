e1a65101339c4335eba80114165463ea4b6ab0a7
Reed Dunkle <reeddunkle@gmail.com>
2018-10-03 11:54:48 -0400

    WIP: looking good

--- src/components/atoms/form/RadioButton.jsx
+++ src/components/atoms/form/RadioButton.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import nanoid from 'nanoid';
-import FormField from './FormField';
 
 const Label = styled.label`
-  display: block;
+  display: inline-block;
 `;
 
 const RadioInput = styled.input.attrs({
@@ -15,12 +14,10 @@ const RadioInput = styled.input.attrs({
 
 function RadioButton({ id = nanoid(), label, ...rest }) {
   return (
-    <FormField>
-      <Label htmlFor={id}>
-        <RadioInput {...rest} id={id} />
-        {label}
-      </Label>
-    </FormField>
+    <Label htmlFor={id}>
+      <RadioInput {...rest} id={id} />
+      {label}
+    </Label>
   );
 }
 
