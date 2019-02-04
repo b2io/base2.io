3ea7cd3fd0323ea7af63da5f52f07e4939db92a9
Mandy Kendall <mlkendall93@gmail.com>
2019-02-04 09:17:20 -0500

    Bottom Margin on BlockQuote

--- src/components/atoms/Blockquote.jsx
+++ src/components/atoms/Blockquote.jsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
-import { em } from 'polished';
+import { em, rem } from 'polished';
 import { themed } from '../../util/style';
 
 const Blockquote = styled.blockquote`
   border-left: ${em('10px')} solid ${themed('color.border')};
   font-size: 1.5em;
   line-height: 1.35;
-  margin: 0;
+  margin: 0 0 ${rem('21px')};
   padding: ${em('10px')} ${em('40px')};
 `;
 
