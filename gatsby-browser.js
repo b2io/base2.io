e84103186661deadda4feac40bb6c8c073e2ab26
Tony Borres <tborres@base2.io>
2019-06-06 10:04:42 -0400

    WIP: rollback dynamic polyfills

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,7 @@
-exports.onClientEntry = async () => {
+/* eslint-disable global-require */
+exports.onClientEntry = () => {
   // Polyfills:
-  if (typeof fetch === 'undefined') {
-    await import('whatwg-fetch');
-  }
-  if (typeof Object.assign === 'undefined') {
-    Object.assign = await import('object-assign');
-  }
+  require('whatwg-fetch');
+  Object.assign = require('object-assign');
 };
+/* eslint-enable global-require */
