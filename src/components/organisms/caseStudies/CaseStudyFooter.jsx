486ac9236ef9301cbdd0591b40d78caaf1eefd0a
chriswhawkins <chawkins@base2.io>
2019-10-24 15:33:44 -0400

    wip footer background

--- src/components/organisms/caseStudies/CaseStudyFooter.jsx
+++ src/components/organisms/caseStudies/CaseStudyFooter.jsx
@@ -4,10 +4,13 @@ import { Button } from '../../atoms';
 import { webpBackground } from '../../../util/style';
 
 const Wrapper = styled.div`
+  align-items: center;
   background: no-repeat center;
-  background-size: 10% 100%;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  min-height: 45vw;
   overflow-x: hidden;
-  min-height: 150px;
   width: 100%;
 
   ${webpBackground('/img/backgrounds/moon.jpg', '/img/backgrounds/moon.webp')};
