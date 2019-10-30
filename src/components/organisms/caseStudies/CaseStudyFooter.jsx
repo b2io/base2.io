f3e97c08774e1f53144c9d0e1e1b905499e581dd
k8hansen <k8hansen@gmail.com>
2019-10-30 13:48:56 -0400

    fixed spacing on button and image in case studies footer

--- src/components/organisms/caseStudies/CaseStudyFooter.jsx
+++ src/components/organisms/caseStudies/CaseStudyFooter.jsx
@@ -6,11 +6,13 @@ import { em } from 'polished';
 import { CallToActionLink } from '../../atoms';
 
 const Wrapper = styled.div`
-  align-items: center;
-  background: no-repeat center;
+  align-items: flex-start;
+  padding-top: 4vh;
+  background: no-repeat bottom;
   background-size: cover;
   display: flex;
   justify-content: center;
+  margin-top: 5vw;
   min-height: 45vw;
   overflow-x: hidden;
   width: 100%;
