5e64532c2626b640544a7a90191acb9f15a42073
Nathan Kessler <nkessler@base2.io>
2020-03-12 08:46:51 -0400

    Fix ruby highlighting in SSR

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,18 +5,23 @@ import { renderToString } from 'react-dom/server';
 import { ServerStyleSheet, ThemeProvider } from 'styled-components';
 import { darkTheme } from './src/theme';
 import WrapWithGlobalStyles from './wrap-with-global-styles';
+import './src/util/prism-languages';
 
 export const replaceRenderer = ({
   bodyComponent,
   replaceBodyHTMLString,
   setHeadComponents,
 }) => {
-  replaceBodyHTMLString(renderToString(<WrapWithGlobalStyles element={bodyComponent} />));
+  replaceBodyHTMLString(
+    renderToString(<WrapWithGlobalStyles element={bodyComponent} />),
+  );
 
   // Add styled-components in SSR/build
   const sheet = new ServerStyleSheet();
   try {
-    const html = renderToString(sheet.collectStyles(<WrapWithGlobalStyles element={bodyComponent} />));
+    const html = renderToString(
+      sheet.collectStyles(<WrapWithGlobalStyles element={bodyComponent} />),
+    );
     const styleElement = sheet.getStyleElement();
     setHeadComponents(styleElement);
   } catch (error) {
