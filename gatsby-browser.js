e7e57ce1283f589149bdfaff5e34f407f44bc565
Nathan Kessler <nathant93@gmail.com>
2020-05-18 08:35:48 -0400

    fix(site): Streamline polyfills (#487)
    Close #393, Close #371

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,12 @@
 import wrapWithGlobalStyles from './wrap-with-global-styles';
 
 export const onClientEntry = () => {
-  // Polyfills:
-  require('whatwg-fetch');
-  require('./src/util/prism-languages');
-  Object.assign = require('object-assign');
+  if (!CustomEvent) {
+    require('custom-event-polyfill');
+  }
+  if (!document.getElementsByTagName('html')[0].closest) {
+    require('element-closest-polyfill');
+  }
 };
 
 export const wrapRootElement = wrapWithGlobalStyles;
