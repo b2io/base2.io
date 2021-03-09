db709e2f342a00d64bf2d71315228688118d1bdd
Tony Borres <tborres@base2.io>
2021-03-09 09:23:48 -0500

    WIP: basic component layout

---
+++ src/components/CTA.tsx
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+import Link from 'next/link';
+import React, { FC } from 'react';
+
+export type CTAProps = {
+  href: string;
+};
+
+const A = styled.a(({ theme }) => ({
+  '::after': {
+    content: '""',
+    display: 'block',
+    width: '100%',
+    marginTop: 8,
+    height: 2,
+    backgroundColor: theme.colors.action,
+  },
+  color: theme.colors.text,
+  fontSize: 22,
+  fontWeight: 900,
+  lineHeight: '22px',
+}));
+
+export const CTA: FC<CTAProps> = ({ children, href }) => {
+  return (
+    <Link href={href}>
+      <A>{children}</A>
+    </Link>
+  );
+};
+
+export default CTA;
