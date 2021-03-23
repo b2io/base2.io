981f9de0973742752c08a3cae775b53c9304216a
Tony Borres <tborres@base2.io>
2021-03-23 12:08:14 -0400

    WIP: transform into close button

--- src/components/icons/MenuIcon.tsx
+++ src/components/icons/MenuIcon.tsx
@@ -11,31 +11,51 @@ const Container = styled.div`
   position: relative;
   width: 24px;
   span {
-    background-color: ${(props: MenuIconProps) =>
-      props.isClose ? 'red' : 'white'};
+    background-color: ${({ theme }) => theme.colors.offWhite};
     height: 1px;
     position: absolute;
     right: 0;
     width: 100%;
 
     &:nth-of-type(1) {
+      transform-origin: top right;
       width: 19px;
     }
 
     &:nth-of-type(2) {
       top: 5px;
+      transform-origin: 100% 50%;
       width: 16px;
     }
 
     &:nth-of-type(3) {
       bottom: 0;
+      transform-origin: bottom right;
+    }
+  }
+
+  &.close {
+    span {
+      width: 19px;
+
+      &:nth-of-type(1) {
+        transform: rotate(-45deg) scaleX(1.36125) translate(0px, -5px);
+      }
+
+      &:nth-of-type(2) {
+        display: none;
+      }
+
+      &:nth-of-type(3) {
+        transform: rotate(45deg) scaleX(1.36125) translate(0px, 5px);
+      }
     }
   }
 `;
 
-export const MenuIcon: FC<MenuIconProps> = ({ ...props }) => {
+export const MenuIcon: FC<MenuIconProps> = ({ isClose, ...props }) => {
   return (
-    <Container {...props}>
+    <Container {...props} className={isClose ? 'close' : ''}>
       <span />
       <span />
       <span />
