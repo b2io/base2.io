44e771a82c935b19aeaea6b42692160a636ae30f
Tony Borres <tborres@base2.io>
2021-04-13 13:30:14 -0400

    WIP: attempt to resize image using cssClamp

--- src/components/panes/DesignedForHumansPane.tsx
+++ src/components/panes/DesignedForHumansPane.tsx
@@ -1,17 +1,18 @@
 import css from '@emotion/css';
-import { FC } from 'react';
 import Image from 'next/image';
+import { FC } from 'react';
 import { Heading, Text } from '~/components';
+import { cssClamp } from '~/theme';
 
 export const DesignedForHumansPane: FC = () => {
   return (
     <div>
       <div
         css={css`
-          height: 20.1875em;
-          left: calc(50% - 50vw - 5.8545em);
+          height: ${cssClamp([20.188, 'xs'], [26.625, 'md'], [50, 'lg'])};
+          left: calc(50% - 50vw - ${cssClamp([5.8545, 'xs'], [0, 'md'])});
           position: relative;
-          width: 26.042em;
+          width: ${cssClamp([26.042, 'xs'], [34.313, 'md'], [64.5, 'lg'])};
         `}
       >
         <Image
