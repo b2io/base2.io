f645e2c8fe398b3be36f62273877d1fce6c49fd7
Nelson Foltz <nfoltz@base2.io>
2021-04-23 11:29:39 -0400

    add: FueledByCuriosity component and render via index

---
+++ src/components/pages/home/FueledByCuriosity.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+export const FueledByCuriosity: FC = () => {
+  return <div>hello from fueled by curiosity</div>;
+};
+
+export default FueledByCuriosity;
--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -4,6 +4,7 @@ import { Layout } from '~/components';
 import BuiltForResults from './BuiltForResults';
 import DesignedForHumans from './DesignedForHumans';
 import FeaturedCaseStudy from './FeaturedCaseStudy';
+import FueledByCuriosity from './FueledByCuriosity';
 import HomeHero from './HomeHero';
 
 const Home: FC = () => {
@@ -13,6 +14,7 @@ const Home: FC = () => {
       <DesignedForHumans />
       <BuiltForResults />
       <FeaturedCaseStudy />
+      <FueledByCuriosity />
     </Layout>
   );
 };
