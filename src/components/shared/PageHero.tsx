8db2334e0808d2e52f484250f0596515ad09a9f1
Nelson Foltz <nfoltz@base2.io>
2021-04-28 14:36:06 -0400

    add: test props passed down and working

--- src/components/shared/PageHero.tsx
+++ src/components/shared/PageHero.tsx
@@ -1,15 +1,17 @@
 import { FC } from 'react';
 import { Heading } from '~/components';
+import { ImageProps } from '~/types';
 
 // TODO: move this to types folder
-type PageHeroProps = {
+type PageHeroProps = ImageProps & {
   text: string;
 };
 
-export const PageHero: FC<PageHeroProps> = ({ text }) => {
+export const PageHero: FC<PageHeroProps> = ({ alt, text, src }) => {
   return (
     <section>
       <Heading as="h1">{text}</Heading>
+      <img src={src} alt={alt} />
     </section>
   );
 };
