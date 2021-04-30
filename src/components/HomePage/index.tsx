4eeea2ca0905b5f7f7c5df63cbc749787ff8d811
Nelson Foltz <nfoltz@base2.io>
2021-04-30 08:43:50 -0400

    add: bps passed in from index

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,31 +1,29 @@
 import { FC } from 'react';
 import { Layout } from '~/components';
-
 // import { BuiltForResults } from './BuiltForResults';
 // import { DesignedForHumans } from './DesignedForHumans';
 // import { FeaturedCaseStudy } from './FeaturedCaseStudy';
 import { FueledByCuriosity } from './FueledByCuriosity';
 // import { HomeHero } from './HomeHero';
-
 import { PageHero } from '~/components/shared';
 
-// import { bp } from '~/theme/breakpoints';
+import { bp } from '~/theme/breakpoints';
 
 const heroImageData = [
   {
-    media: '(min-width: 1920px)',
+    media: `(min-width: ${bp.desktop}px)`,
     srcSet: '/shared/approach-hero-1920.jpg',
   },
   {
-    media: '(min-width: 1600px)',
+    media: `(min-width: ${bp.xl}px)`,
     srcSet: '/shared/approach-hero-1600.jpg',
   },
   {
-    media: '(min-width: 768px)',
+    media: `(min-width: ${bp.tablet}px)`,
     srcSet: '/shared/approach-hero-768.jpg',
   },
   {
-    media: '(min-width: 0px)',
+    media: `(min-width: ${bp.xs}px)`,
     srcSet: '/shared/approach-hero-375.jpg',
   },
 ];
