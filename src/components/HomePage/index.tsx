0a398ec77f063f61aa5392faa3775c73bb63b62a
Nelson Foltz <nfoltz@base2.io>
2021-05-04 08:08:19 -0400

    add: PageHero to Approach page and remove from temp location for rendering

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,31 +1,18 @@
 import { FC } from 'react';
 import { Layout } from '~/components';
-// import { BuiltForResults } from './BuiltForResults';
-// import { DesignedForHumans } from './DesignedForHumans';
-// import { FeaturedCaseStudy } from './FeaturedCaseStudy';
+import { BuiltForResults } from './BuiltForResults';
+import { DesignedForHumans } from './DesignedForHumans';
+import { FeaturedCaseStudy } from './FeaturedCaseStudy';
 import { FueledByCuriosity } from './FueledByCuriosity';
-// import { HomeHero } from './HomeHero';
-import { PageHero } from '~/components/shared';
-
-const imgSource = {
-  desktop: '/shared/approach-hero-xlarge.jpg',
-  tablet: '/shared/approach-hero-medium.jpg',
-  xl: '/shared/approach-hero-large.jpg',
-  xs: '/shared/approach-hero-small.jpg',
-};
+import { HomeHero } from './HomeHero';
 
 export const HomePage: FC = () => {
   return (
     <Layout>
-      <PageHero
-        alt="person staring at computer"
-        imgSource={imgSource}
-        text="People motivate what we do and how we do it"
-      />
-      {/* <HomeHero />
+      <HomeHero />
       <DesignedForHumans />
       <BuiltForResults />
-      <FeaturedCaseStudy /> */}
+      <FeaturedCaseStudy />
       <FueledByCuriosity />
     </Layout>
   );
--- src/pages/approach/index.tsx
+++ src/pages/approach/index.tsx
@@ -1,11 +1,23 @@
 import { NextPage } from 'next';
-
 import { Layout } from '~/components';
+
+import { PageHero } from '~/components/shared';
+
+const imgSource = {
+  desktop: '/shared/approach-hero-xlarge.jpg',
+  tablet: '/shared/approach-hero-medium.jpg',
+  xl: '/shared/approach-hero-large.jpg',
+  xs: '/shared/approach-hero-small.jpg',
+};
 
 const ApproachIndexPage: NextPage = () => {
   return (
     <Layout>
-      <h1>Approach challenges require unique solutions.</h1>
+      <PageHero
+        alt="person staring at computer"
+        imgSource={imgSource}
+        text="People motivate what we do and how we do it"
+      />
     </Layout>
   );
 };
