6c05b18f2153f04be7e6d5750f0d5543eaab3169
Nelson Foltz <nfoltz@base2.io>
2021-05-04 08:04:55 -0400

    add: approach folder and basic index page for approach page

---
+++ src/pages/approach/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+
+import { Layout } from '~/components';
+
+const ApproachIndexPage: NextPage = () => {
+  return (
+    <Layout>
+      <h1>Approach challenges require unique solutions.</h1>
+    </Layout>
+  );
+};
+
+export default ApproachIndexPage;
