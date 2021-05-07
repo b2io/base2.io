f2654a3c69ebb797fd0e0fdfb249bce56a0ac12e
Nelson Foltz <nfoltz@base2.io>
2021-05-07 15:43:48 -0400

    update: just nerding out and capitalizing the page name text for consistency

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -4,7 +4,7 @@ import { Layout } from '~/components';
 const ContactIndexPage: NextPage = () => {
   return (
     <Layout title="Contact">
-      <h1>Hello from the contact page</h1>
+      <h1>Hello from the Contact page</h1>
     </Layout>
   );
 };
--- src/pages/work/index.tsx
+++ src/pages/work/index.tsx
@@ -4,7 +4,7 @@ import { Layout } from '~/components';
 const WorkIndexPage: NextPage = () => {
   return (
     <Layout title="Work">
-      <h1>Hello from the work page</h1>
+      <h1>Hello from the Work page</h1>
     </Layout>
   );
 };
