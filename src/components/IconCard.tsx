6f33826e1c103ef49f9ba27e175950c51ef8304a
Ashley Singleton <asingleton@base2.io>
2021-05-07 10:15:13 -0400

    Create Icon Card component

---
+++ src/components/IconCard.tsx
@@ -0,0 +1,87 @@
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import { FC } from 'react';
+
+import { spacing } from '~/theme';
+import { Heading, Text } from '~/components';
+
+import {
+  BusinessIcon,
+  CommunityIcon,
+  DeliveryIcon,
+  DeveloperIcon,
+  DiscoveryIcon,
+  EmbeddedIcon,
+  EmpathyIcon,
+  ExecutionIcon,
+  FullyManagedIcon,
+  SupportIcon,
+  SustainabilityIcon,
+  TransparencyIcon,
+} from './icons';
+
+type Icon =
+  | 'business'
+  | 'community'
+  | 'delivery'
+  | 'developer'
+  | 'discovery'
+  | 'embedded'
+  | 'empathy'
+  | 'execution'
+  | 'fullyManaged'
+  | 'support'
+  | 'sustainability'
+  | 'transparency';
+
+export type IconCardProps = {
+  icon: Icon;
+  heading: string;
+};
+
+const IconImage = styled.div(`
+  svg {
+    width: 50px;
+    margin-bottom: ${spacing.xxs}
+  }
+`);
+
+const IconList = {
+  business: <BusinessIcon />,
+  community: <CommunityIcon />,
+  delivery: <DeliveryIcon />,
+  developer: <DeveloperIcon />,
+  discovery: <DiscoveryIcon />,
+  embedded: <EmbeddedIcon />,
+  empathy: <EmpathyIcon />,
+  execution: <ExecutionIcon />,
+  fullyManaged: <FullyManagedIcon />,
+  support: <SupportIcon />,
+  sustainability: <SustainabilityIcon />,
+  transparency: <TransparencyIcon />,
+};
+
+export const IconCard: FC<IconCardProps> = ({ children, icon, heading }) => {
+  const iconSelected = icon;
+
+  return (
+    <div
+      css={css`
+        width: 50%;
+      `}
+    >
+      <IconImage className={icon}>{IconList[iconSelected]}</IconImage>
+      <Heading
+        as="h3"
+        color="coral"
+        variant="h3"
+        css={css`
+          margin-bottom: ${spacing.xs};
+        `}
+      >
+        {heading}
+      </Heading>
+      <Text>{children}</Text>
+    </div>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ export * from './Container';
 export * from './Header';
 export * from './Heading';
 export * from './HomePage';
+export * from './IconCard';
 export * from './icons';
 export * from './Layout';
 export * from './Link';
