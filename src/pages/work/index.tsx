be8919c9fde7c8c604cf8d72ac51d83f095ca2ba
Nelson Foltz <nfoltz@base2.io>
2021-05-07 15:00:38 -0400

    remove: references to cms from work.index

--- src/pages/work/index.tsx
+++ src/pages/work/index.tsx
@@ -1,34 +1,12 @@
-import { GetStaticProps, NextPage } from 'next';
-import Link from 'next/link';
-
-import { CaseStudy, listCaseStudies } from '~/cms';
+import { NextPage } from 'next';
 import { Layout } from '~/components';
 
-export type WorkIndexPageProps = {
-  caseStudies: CaseStudy[];
-};
-
-const WorkIndexPage: NextPage<WorkIndexPageProps> = ({ caseStudies }) => {
+const WorkIndexPage: NextPage = () => {
   return (
     <Layout title="Work">
       <h1>Unique challenges require unique solutions.</h1>
-      <ul>
-        {caseStudies.map((caseStudy) => (
-          <li key={caseStudy.id}>
-            <Link href={`/work/${caseStudy.slug.join('/')}`}>
-              <a>{caseStudy.title}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
     </Layout>
   );
-};
-
-export const getStaticProps: GetStaticProps<WorkIndexPageProps> = async () => {
-  const caseStudies = await listCaseStudies();
-
-  return { props: { caseStudies } };
 };
 
 export default WorkIndexPage;
