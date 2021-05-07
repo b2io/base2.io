adf26d5e1501fa60e0025e4ce3c3a2360183ce3a
Ashley Singleton <asingleton@base2.io>
2021-05-07 15:46:01 -0400

    Create Icon Card component (#555)
    * Create Icon Card component
    
    * Remove CSS
    
    * WIP: add story and pass props
    
    * Clean up code based on PR request
    
    * Improve icon width layout
    
    Co-authored-by: Tony Borres <tborres@base2.io>

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,6 +3,7 @@ import { spacing } from './spacing';
 import { linkVariants, textVariants } from './typography';
 
 export * from './breakpoints';
+export * from './spacing';
 export type { ThemeColor } from './colors';
 export type { ThemeLinkVariants, ThemeTextVariants } from './typography';
 export * from './util';
