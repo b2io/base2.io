24f9c484b2ff66d2dcff849d73c21c4418a592d0
Nelson Foltz <nfoltz@base2.io>
2021-05-07 15:55:45 -0400

    B2 83 remove cms file (#556)
    * remove: references to cms from work.index
    
    * remove: work slug file
    
    * remove: cms imports in blog index
    
    * add: created contact and culture folders and index files
    
    * remove: cms file
    
    * update: just nerding out and capitalizing the page name text for consistency

---
+++ src/pages/culture/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import { Layout } from '~/components';
+
+const CultureIndexPage: NextPage = () => {
+  return (
+    <Layout title="Culture">
+      <h1>Hello from the Culture page</h1>
+    </Layout>
+  );
+};
+
+export default CultureIndexPage;
