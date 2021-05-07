bcd10559659dc72020e3878ee7055a41c0aefe32
Ashley Singleton <asingleton@base2.io>
2021-05-07 14:34:53 -0400

    Add components for the how we build pane

--- src/pages/approach/index.tsx
+++ src/pages/approach/index.tsx
@@ -1,5 +1,8 @@
 import { NextPage } from 'next';
-import { Layout, PageHero } from '~/components';
+import { css } from '@emotion/react';
+
+import { Heading, Layout, Link, PageHero } from '~/components';
+import { spacing, atMinTablet } from '~/theme';
 
 const heroImageSource = {
   largeDesktop: '/approach/approach-hero-xlarge.jpg',
@@ -15,7 +18,44 @@ const ApproachIndexPage: NextPage = () => {
         alt="person staring at a computer"
         imgSource={heroImageSource}
         text="People motivate what we do and how we do it"
+        css={css`
+          margin-bottom: ${spacing.xxl2};
+
+          ${atMinTablet} {
+            margin-bottom: ${spacing.xxl3};
+          }
+        `}
       />
+
+      <Heading
+        as="h2"
+        variant="h2"
+        css={css`
+          margin-bottom: ${spacing.xs};
+        `}
+      >
+        How we build.
+      </Heading>
+      <Heading
+        as="h3"
+        variant="h3"
+        css={css`
+          margin-bottom: ${spacing.xxl};
+          max-width: 51.938rem;
+
+          ${atMinTablet} {
+            margin-bottom: ${spacing.xxl2};
+          }
+        `}
+      >
+        Let&apos;s face it—things pop up along the way. Our development process
+        is intentionally flexible to allow us to accomodate the unexpected.
+        While we work with everyone uniquely, a few steps are central to our
+        approach.
+      </Heading>
+      <Link href="" variant="CTA">
+        Let&apos;s make a plan together
+      </Link>
     </Layout>
   );
 };
