eea8378421bc1222d9ead9aefa313fdfd4ac5cc3
Ashley Singleton <asingleton@base2.io>
2021-05-10 15:43:31 -0400

    Create icon row styled component

--- src/pages/approach/HowWeBuild.tsx
+++ src/pages/approach/HowWeBuild.tsx
@@ -1,8 +1,27 @@
 import { NextPage } from 'next';
 import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 
 import { Heading, IconCard, Link } from '~/components';
-import { spacing, atMinTablet, atMinDesktop } from '~/theme';
+import { spacing, atMinTablet, atMinDesktop, cssClamp } from '~/theme';
+
+const IconRow = styled.div`
+  display: flex;
+
+  > div {
+    margin-bottom: ${spacing.xxl};
+    max-width: 20.625rem;
+    max-width: ${cssClamp([20.625, 'mobile'], [29.063, 'desktop'])};
+  }
+
+  > :nth-child(odd) {
+    margin-right: ${spacing.sm};
+
+    ${atMinDesktop} {
+      margin-right: 16.688rem;
+    }
+  }
+`;
 
 export const HowWeBuild: NextPage = () => {
   return (
@@ -33,30 +52,7 @@ export const HowWeBuild: NextPage = () => {
         While we work with everyone uniquely, a few steps are central to our
         approach.
       </Heading>
-      <div
-        css={css`
-          display: flex;
-          flex-flow: row wrap;
-
-          > div {
-            flex: 1 1 auto;
-            margin-bottom: ${spacing.xxl};
-            max-width: 20.625rem;
-
-            ${atMinDesktop} {
-              max-width: 29.063rem;
-            }
-          }
-
-          > :nth-child(odd) {
-            margin-right: 1.875rem;
-
-            ${atMinDesktop} {
-              margin-right: 16.688rem;
-            }
-          }
-        `}
-      >
+      <IconRow>
         <IconCard icon="discovery" heading="Discovery">
           Your idea forms the basis of our process. We work with you to fully
           understand the story you want communicated or the problem you want
@@ -67,6 +63,8 @@ export const HowWeBuild: NextPage = () => {
           two-week development sprints, daily standups, demos of completed
           tasks, and retrospectives for feedback and course correction.
         </IconCard>
+      </IconRow>
+      <IconRow>
         <IconCard icon="delivery" heading="Delivery">
           At the end of each sprint, we release software and gather user
           feedback to better shape it moving forward. We also debut completed
@@ -79,7 +77,7 @@ export const HowWeBuild: NextPage = () => {
           and make any necessary adjustments to the structure, flow and
           documentation.
         </IconCard>
-      </div>
+      </IconRow>
       <Link href="" variant="CTA">
         Let&apos;s make a plan together
       </Link>
