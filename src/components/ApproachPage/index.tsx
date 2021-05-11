6cd8d766f177a76e7f4e0865311b63ffb946d0af
Ashley Singleton <asingleton@base2.io>
2021-05-11 10:23:27 -0400

    B2 76 how we engage pane (#560)
    * Add headings and links for engagement pane
    
    * Add spacing between components
    
    * Add Icon Card, section wrapper, and new spacing variable
    
    * Move section to separate file and then import in index
    
    * WIP: adjust approach page file structure
    
    * WIP: add link to contact, and fix typo in unrelated page
    
    * Add side margin on larger screens
    
    Co-authored-by: Tony Borres <tborres@base2.io>

---
+++ src/components/ApproachPage/index.tsx
@@ -0,0 +1,33 @@
+import { css } from '@emotion/react';
+import React, { FC } from 'react';
+import { Layout, PageHero } from '~/components';
+import { atMinTablet } from '~/theme';
+import { spacing } from '~/theme/spacing';
+import { HowWeEngage } from './HowWeEngage';
+
+const heroImageSource = {
+  largeDesktop: '/approach/approach-hero-xlarge.jpg',
+  tablet: '/approach/approach-hero-medium.jpg',
+  xl: '/approach/approach-hero-large.jpg',
+  xs: '/approach/approach-hero-small.jpg',
+};
+
+export const ApproachPage: FC = () => {
+  return (
+    <Layout>
+      <PageHero
+        alt="person staring at a computer"
+        imgSource={heroImageSource}
+        text="People motivate what we do and how we do it."
+        css={css`
+          margin-bottom: ${spacing.xxl2};
+
+          ${atMinTablet} {
+            margin-bottom: ${spacing.xxl3};
+          }
+        `}
+      />
+      <HowWeEngage />
+    </Layout>
+  );
+};
