5a4f7af166f31ae8dc1904a78ddac32762ab334a
Nelson Foltz <nfoltz@base2.io>
2021-05-12 14:05:55 -0400

    add: basic card structure

--- src/components/TeamMemberCard.tsx
+++ src/components/TeamMemberCard.tsx
@@ -1,27 +1,36 @@
 import { FC } from 'react';
-// import NextImage from 'next/image';
+import NextImage from 'next/image';
 
-// type TeamMemberCardProps = {
-//   alt: string;
-//   src: string;
-//   position: string;
-//   name: string;
-// };
+type TeamMemberCardProps = {
+  alt: string;
+  src: string;
+  position: string;
+  name: string;
+};
 
-// const ResponsiveImage: FC<TeamMemberCardProps> = ({ alt, src, ...props }) => {
-//   return (
-//     <div {...props}>
-//       <NextImage
-//         alt={alt}
-//         height={706}
-//         layout="responsive"
-//         src={src}
-//         width={400}
-//       />
-//     </div>
-//   );
-// };
+const ResponsiveImage: FC<TeamMemberCardProps> = ({ alt, src, ...props }) => {
+  return (
+    <div {...props}>
+      <NextImage
+        alt={alt}
+        height={600}
+        layout="responsive"
+        src={src}
+        width={465}
+      />
+    </div>
+  );
+};
 
 export const TeamMemberCard: FC = () => {
-  return <div>hello from team card</div>;
+  return (
+    <>
+      <ResponsiveImage
+        alt="Drew Miller"
+        name="Drew Miller"
+        position="Owner &amp; Principal Software Engineer"
+        src="/culture/dmiller.jpg"
+      />
+    </>
+  );
 };
