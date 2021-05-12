be04f8463a781c72bb873e4c2967b9f3cb3a40b3
Nelson Foltz <nfoltz@base2.io>
2021-05-12 10:37:46 -0400

    update: cropped images without the white lines at top and bottom

--- src/components/CulturePage/index.tsx
+++ src/components/CulturePage/index.tsx
@@ -2,17 +2,17 @@ import React, { FC } from 'react';
 import { Layout, PageHero } from '~/components';
 
 const heroImageSource = {
-  largeDesktop: '/approach/approach-hero-xlarge.jpg',
-  tablet: '/approach/approach-hero-medium.jpg',
-  xl: '/approach/approach-hero-large.jpg',
-  xs: '/approach/approach-hero-small.jpg',
+  largeDesktop: '/culture/culture-hero-xlarge.jpg',
+  tablet: '/culture/culture-hero-medium.jpg',
+  xl: '/culture/culture-hero-large.jpg',
+  xs: '/culture/culture-hero-small.jpg',
 };
 
 export const CulturePage: FC = () => {
   return (
     <Layout>
       <PageHero
-        alt="person staring at a computer"
+        alt="people sitting around conference table collaborating"
         imgSource={heroImageSource}
         text="We are engineers and artisans, collaborating to make custom software people love."
       />
