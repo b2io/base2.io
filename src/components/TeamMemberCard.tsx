006c85e65e8fe2f9da814f37fc88cfc8a109851d
Nelson Foltz <nfoltz@base2.io>
2021-05-12 11:35:05 -0400

    add: basic scaffold

--- src/components/TeamMemberCard.tsx
+++ src/components/TeamMemberCard.tsx
@@ -1,4 +1,26 @@
 import { FC } from 'react';
+// import NextImage from 'next/image';
+
+// type TeamMemberCardProps = {
+//   alt: string;
+//   src: string;
+//   position: string;
+//   name: string;
+// };
+
+// const ResponsiveImage: FC<TeamMemberCardProps> = ({ alt, src, ...props }) => {
+//   return (
+//     <div {...props}>
+//       <NextImage
+//         alt={alt}
+//         height={706}
+//         layout="responsive"
+//         src={src}
+//         width={400}
+//       />
+//     </div>
+//   );
+// };
 
 export const TeamMemberCard: FC = () => {
   return <div>hello from team card</div>;
