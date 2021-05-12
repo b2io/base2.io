354fd71bedcdeac1b181c97df7c796d53d181363
Nelson Foltz <nfoltz@base2.io>
2021-05-12 10:08:34 -0400

    add: culture page in components

---
+++ src/components/CulturePage/index.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from 'react';
+import { Layout, PageHero } from '~/components';
+
+const heroImageSource = {
+  largeDesktop: '/approach/approach-hero-xlarge.jpg',
+  tablet: '/approach/approach-hero-medium.jpg',
+  xl: '/approach/approach-hero-large.jpg',
+  xs: '/approach/approach-hero-small.jpg',
+};
+
+export const CulturePage: FC = () => {
+  return (
+    <Layout>
+      <PageHero
+        alt="person staring at a computer"
+        imgSource={heroImageSource}
+        text="People motivate what we do and how we do it."
+      />
+    </Layout>
+  );
+};
