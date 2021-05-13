a39ce59e1a2e8a78399d6c6f4bd4344c1262574d
Drew Miller <dmiller@base2.io>
2021-05-13 11:31:27 -0400

    Add open positions text and CTA to Culture page (#566)
    B2-18

--- src/components/CulturePage/index.tsx
+++ src/components/CulturePage/index.tsx
@@ -1,21 +1,55 @@
-import React, { FC } from 'react';
-import { Layout, PageHero } from '~/components';
+import { css } from '@emotion/react';
+import type { FC } from 'react';
 
-const heroImageSource = {
-  largeDesktop: '/culture/culture-hero-xlarge.jpg',
-  tablet: '/culture/culture-hero-medium.jpg',
-  xl: '/culture/culture-hero-large.jpg',
-  xs: '/culture/culture-hero-small.jpg',
-};
+import { Heading } from '../Heading';
+import { Layout } from '../Layout';
+import { Link } from '../Link';
+import { PageHero } from '../PageHero';
+import { Text } from '../Text';
 
-export const CulturePage: FC = () => {
+export type CulturePageProps = Record<string, never>;
+
+export const CulturePage: FC<CulturePageProps> = () => {
   return (
-    <Layout>
+    <Layout title="Culture">
       <PageHero
         alt="people sitting around conference table collaborating"
-        imgSource={heroImageSource}
+        imgSource={{
+          largeDesktop: '/culture/culture-hero-xlarge.jpg',
+          tablet: '/culture/culture-hero-medium.jpg',
+          xl: '/culture/culture-hero-large.jpg',
+          xs: '/culture/culture-hero-small.jpg',
+        }}
         text="We are engineers and artisans, collaborating to make custom software people love."
       />
+      <section
+        css={css`
+          display: grid;
+          gap: 1.5rem;
+          justify-items: start;
+          margin-top: 12.5rem;
+          max-width: 52rem;
+        `}
+      >
+        <Heading as="h2" variant="h2">
+          Let&rsquo;s turn ideas into beautiful software
+        </Heading>
+        <Text as="p" variant="h3">
+          Feeling us? We&rsquo;re always looking for like-minded people with
+          similar values and development chops. Our team is tight-knit,
+          seasoned, and intentionally small&mdash;allowing everyone&rsquo;s
+          strengths to shine.
+        </Text>
+        <Link
+          css={css`
+            margin-top: 1rem;
+          `}
+          href="/culture"
+          variant="CTA"
+        >
+          See all available positions
+        </Link>
+      </section>
     </Layout>
   );
 };
