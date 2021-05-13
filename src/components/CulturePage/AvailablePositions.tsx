d0335896d071729c5c768494315a5f0ecc1d70aa
Drew Miller <dmiller@base2.io>
2021-05-13 12:54:17 -0400

    Add Our Culture & Beliefs content to Culture page
    - Add OurCulture content to CulturePage
    - Extract pane component for AvailablePositions
    - Fix destination of AvailablePositions CTA
    
    B2-89, B2-93

---
+++ src/components/CulturePage/AvailablePositions.tsx
@@ -0,0 +1,39 @@
+import { css } from '@emotion/react';
+import type { FC } from 'react';
+
+import { Heading } from '../Heading';
+import { Link } from '../Link';
+import { Text } from '../Text';
+
+export const AvailablePositions: FC = (props) => {
+  return (
+    <section
+      css={css`
+        display: grid;
+        gap: 1.5rem;
+        justify-items: start;
+        max-width: 52rem;
+      `}
+      {...props}
+    >
+      <Heading as="h2" variant="h2">
+        Let&rsquo;s turn ideas into beautiful software
+      </Heading>
+      <Text as="p" variant="h3">
+        Feeling us? We&rsquo;re always looking for like-minded people with
+        similar values and development chops. Our team is tight-knit, seasoned,
+        and intentionally small&mdash;allowing everyone&rsquo;s strengths to
+        shine.
+      </Text>
+      <Link
+        css={css`
+          margin-top: 1rem;
+        `}
+        href="/careers"
+        variant="CTA"
+      >
+        See all available positions
+      </Link>
+    </section>
+  );
+};
