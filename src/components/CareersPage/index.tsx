8f0e7b9b10899bb0620c0031fa2de1aff124f2e7
Nelson Foltz <nfoltz@base2.io>
2021-06-04 11:23:47 -0400

    add: CareersPage PageHero

--- src/components/CareersPage/index.tsx
+++ src/components/CareersPage/index.tsx
@@ -1,8 +1,22 @@
-// import { css } from '@emotion/react';
-import React, { FC } from 'react';
+import { FC } from 'react';
 
 import { Layout } from '~/components';
 
+import { PageHero } from '../PageHero';
+
 export const CareersPage: FC = () => {
-  return <Layout title="Careers">careers and stuff</Layout>;
+  return (
+    <Layout title="Careers">
+      <PageHero
+        alt="person sitting at computer and smiling"
+        imgSources={[
+          ['largeDesktop', '/careers/careers-hero-xlarge.jpg'],
+          ['tablet', '/careers/careers-hero-medium.jpg'],
+          ['xl', '/careers/careers-hero-large.jpg'],
+          ['xs', '/careers/careers-hero-small.jpg'],
+        ]}
+        text="Solving seemingly impervious challenges."
+      />
+    </Layout>
+  );
 };
