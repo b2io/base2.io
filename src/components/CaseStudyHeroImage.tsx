435a848cec151c2c829d618558e67a4239b4dc1c
Nelson Foltz <nfoltz@base2.io>
2021-06-07 14:29:23 -0400

    add: max-width and margin for imageContainer

--- src/components/CaseStudyHeroImage.tsx
+++ src/components/CaseStudyHeroImage.tsx
@@ -1,12 +1,16 @@
 import styled from '@emotion/styled';
 import type { FC } from 'react';
 
-import { cssClamp } from '~/theme';
+import { atMinLg, cssClamp } from '~/theme';
 import type { ImageProps } from '~/types';
 
 const ImageContainer = styled.div`
   height: 20.188rem;
   height: ${cssClamp([20.188, 'smMobile'], [50, 'desktop'])};
+  ${atMinLg} {
+    margin: auto;
+    max-width: 74.875rem;
+  }
 `;
 
 const Image = styled.img`
