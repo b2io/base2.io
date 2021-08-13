a8d40a3b5b6174e80f19ef9795ba35921b81a0df
Laura Padilla <hi@lauracodes.com>
2021-08-13 17:11:39 -0400

    WIP: Finish contact form + validations

--- src/components/ContactPage/ContactForm.tsx
+++ src/components/ContactPage/ContactForm.tsx
@@ -1,14 +1,24 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { useForm, ValidationError } from '@formspree/react';
-import type { FC } from 'react';
+import { FC } from 'react';
+import { useForm } from 'react-hook-form';
+import type { SubmitHandler } from 'react-hook-form';
 
 import { atMinTablet, colors, cssClamp, spacing } from '~/theme';
 
 import { Heading } from '../Heading';
 import { Text } from '../Text';
 
-const FormWrapper = styled.section`
+const FORM_ID = 'https://formspree.io/f/xvodvoer';
+
+type FormValues = {
+  fullName: string;
+  email: string;
+  message: string;
+  submit: boolean;
+};
+
+const ContactWrapper = styled.section`
   display: grid;
   grid-template-columns: 1fr;
 
@@ -17,18 +27,23 @@ const FormWrapper = styled.section`
   }
 `;
 
+const InputWrapper = styled.div`
+  margin-bottom: ${spacing.xxl2};
+  min-height: 12rem;
+`;
+
 const Label = styled(Heading)`
   margin-bottom: ${spacing.lg};
   font-size: ${cssClamp([2.5, 'mobile'], [3.125, 'tablet'])};
   font-variation-settings: 'wght' 700;
 `;
 
-const Input = styled.input`
+const InputStyles = css`
   color: white;
   font-size: 1.875rem;
   width: 100%;
   height: 4rem;
-  margin-bottom: 6.5rem;
+  margin-bottom: ${spacing.xs};
   background: none;
   border: none;
   border-bottom: 1px solid white;
@@ -48,7 +63,15 @@ const Input = styled.input`
   }
 `;
 
-const FormAside = styled.aside`
+const Input = styled.input`
+  ${InputStyles}
+`;
+
+const Textarea = styled.textarea`
+  ${InputStyles}
+`;
+
+const ContactAside = styled.aside`
   margin-bottom: 6.5rem;
   ${atMinTablet} {
     margin-bottom: 0;
@@ -59,7 +82,7 @@ const ContactInfo = styled.article`
   margin-bottom: ${cssClamp([3, 'mobile'], [5, 'tablet'])};
 `;
 
-const Submit = styled.button`
+const Button = css`
   background: transparent;
   border: 1px solid ${colors.coral};
   font-size: 1.375rem;
@@ -73,59 +96,132 @@ const Submit = styled.button`
   }
 `;
 
-const SubmitMessage = styled.article`
+const Submit = styled.button`
+  ${Button}
+`;
+
+const Error = styled.p`
+  color: ${colors.coral};
+  margin: 0;
+`;
+
+const ThankYouMessage = styled.article`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
+  text-align: center;
+`;
+
+const SendAnotherMsg = styled.button`
+  ${Button}
 `;
 
 export const Form: FC = () => {
-  const [state, handleSubmit] = useForm('mbjqznky');
-  if (state.succeeded) {
-    return (
-      <SubmitMessage>
-        <Heading variant="h3">
-          Thanks for your message! We&apos;ll be in touch soon.
-        </Heading>
-      </SubmitMessage>
-    );
-  }
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitting, errors, isSubmitSuccessful },
+  } = useForm<FormValues>({
+    mode: 'onBlur',
+    reValidateMode: 'onBlur',
+    shouldUseNativeValidation: false,
+  });
+
+  const onSubmit: SubmitHandler<FormValues> = async (data) => {
+    try {
+      await fetch(FORM_ID, {
+        body: JSON.stringify(data),
+        cache: 'no-cache',
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+        method: 'POST',
+        mode: 'cors',
+      });
+      reset();
+    } catch (error) {
+      // catch errors
+    }
+  };
+
   return (
-    <form onSubmit={handleSubmit} autoComplete="off">
-      <Label>Hello, my name is:</Label>
-      <Input
-        id="name"
-        type="name"
-        name="name"
-        placeholder="Name*"
-        required
-        autoComplete="off"
-      />
-      <ValidationError prefix="Name" field="name" errors={state.errors} />
-      <Label>My email is:</Label>
-      <Input
-        id="email"
-        type="email"
-        name="email"
-        placeholder="Email*"
-        required
-        autoComplete="off"
-      />
-      <ValidationError prefix="Email" field="email" errors={state.errors} />
-      <Label>I&apos;d like to talk about:</Label>
-      <Input id="message" name="message" placeholder="Message*" required />
-      <ValidationError prefix="Message" field="message" errors={state.errors} />
-      <Submit type="submit" disabled={state.submitting}>
-        Submit
-      </Submit>
-    </form>
+    <>
+      {isSubmitSuccessful ? (
+        <ThankYouMessage>
+          <Heading
+            variant="h3"
+            css={css`
+              margin-bottom: ${spacing.lg};
+            `}
+          >
+            Thanks for your message! We&apos;ll be in touch soon.
+          </Heading>
+          <SendAnotherMsg type="button" onClick={() => reset()}>
+            Send another message?
+          </SendAnotherMsg>
+        </ThankYouMessage>
+      ) : (
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+          <InputWrapper>
+            <Label>Hello, my name is:</Label>
+            <Input
+              id="name"
+              type="name"
+              placeholder="Name*"
+              required
+              autoComplete="off"
+              {...register('fullName', {
+                required: 'Please enter your name.',
+              })}
+            />
+            {errors.fullName && <Error>{errors.fullName.message}</Error>}
+          </InputWrapper>
+          <InputWrapper>
+            <Label>My email is:</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="Email*"
+              required
+              autoComplete="off"
+              {...register('email', {
+                required: 'Please enter your email address.',
+              })}
+            />
+            {errors.email && <Error>{errors.email.message}</Error>}
+          </InputWrapper>
+          <InputWrapper
+            css={css`
+              min-height: 12.625rem;
+              margin-bottom: ${spacing.md};
+            `}
+          >
+            <Label>I&apos;d like to talk about:</Label>
+            <Textarea
+              id="message"
+              placeholder="Message*"
+              required
+              {...register('message', {
+                required: 'Please write a message.',
+              })}
+            />
+            {errors.message && <Error>{errors.message.message}</Error>}
+          </InputWrapper>
+          <Submit type="submit" disabled={isSubmitting}>
+            Submit
+          </Submit>
+        </form>
+      )}
+    </>
   );
 };
 
 export const Contact: FC = () => {
   return (
-    <FormWrapper>
-      <FormAside>
+    <ContactWrapper>
+      <ContactAside>
         <ContactInfo>
           <Heading
             variant="h3"
@@ -172,8 +268,8 @@ export const Contact: FC = () => {
             </address>
           </Text>
         </ContactInfo>
-      </FormAside>
+      </ContactAside>
       <Form />
-    </FormWrapper>
+    </ContactWrapper>
   );
 };
