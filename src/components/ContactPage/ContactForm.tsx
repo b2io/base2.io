24c9277d54fa881358b4f8b56b794e7287e023eb
Laura Padilla <hi@lauracodes.com>
2021-08-30 15:24:08 -0400

    WIP: update contact page blurb and fix autofill

--- src/components/ContactPage/ContactForm.tsx
+++ src/components/ContactPage/ContactForm.tsx
@@ -55,6 +55,10 @@ const FormInput = styled.input`
   &::placeholder {
     font-size: 1rem;
   }
+  &:-webkit-autofill {
+    box-shadow: 0 0 0px 1000px ${colors.darkBlue} inset;
+    transition: background-color 5000s ease-in-out 0s;
+  }
 `;
 
 const ContactAside = styled.aside`
