1415035155db8d3b5fd5596783e9cfc577ccbff1
Laura Padilla <hi@lauracodes.com>
2021-08-31 12:59:37 -0400

    Fix: adjust autofill css

--- src/components/ContactPage/ContactForm.tsx
+++ src/components/ContactPage/ContactForm.tsx
@@ -55,8 +55,9 @@ const FormInput = styled.input`
   &::placeholder {
     font-size: 1rem;
   }
-  &:autofill,
-  :-webkit-autofill {
+
+  &:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0px 1000px ${colors.darkBlue} inset;
     box-shadow: 0 0 0px 1000px ${colors.darkBlue} inset;
     -webkit-text-fill-color: ${colors.offWhite};
     transition: background-color 5000s ease-in-out 0s;
