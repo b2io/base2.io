9be3b7936de0f8cd05c79bdae0aa46cb51bfe51e
Laura Padilla <hi@lauracodes.com>
2021-08-31 15:24:31 -0400

    Fix: cleanup autofill css

--- src/components/ContactPage/ContactForm.tsx
+++ src/components/ContactPage/ContactForm.tsx
@@ -57,7 +57,6 @@ const FormInput = styled.input`
   }
 
   &:-webkit-autofill {
-    -webkit-box-shadow: 0 0 0px 1000px ${colors.darkBlue} inset;
     box-shadow: 0 0 0px 1000px ${colors.darkBlue} inset;
     -webkit-text-fill-color: ${colors.offWhite};
     transition: background-color 5000s ease-in-out 0s;
