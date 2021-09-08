70ad8eb78bfe56de8aed2611d9aa6b39c07c2147
Laura Padilla <hi@lauracodes.com>
2021-09-08 11:15:59 -0400

    WIP: Add DashIcon

---
+++ src/components/icons/DashIcon.tsx
@@ -0,0 +1,28 @@
+import { useId } from '@react-aria/utils';
+import type { FC } from 'react';
+
+import { SvgProps } from './types';
+
+export const DashIcon: FC<SvgProps> = ({
+  height = '100%',
+  id,
+  title = 'Transparency',
+  width = '100%',
+  ...props
+}) => {
+  const titleId = useId(id);
+
+  return (
+    <svg
+      aria-labelledby={titleId}
+      height={height}
+      role="img"
+      viewBox="0 0 70 50"
+      width={width}
+      {...props}
+    >
+      <title id={titleId}>{title}</title>
+      <rect width="40" height="4" fill="#3F51B5" />
+    </svg>
+  );
+};
