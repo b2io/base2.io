4a1590d33831a5f05257058f0595151258735719
k8hansen@gmail.com <k8hansen@gmail.com>
2021-09-10 14:08:48 -0400

    WIP

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -15,7 +15,10 @@ export type HomePageProps = Record<string, unknown>;
 export const HomePage: FC<HomePageProps> = () => {
   return (
     <Layout
-      description="Software that moves people. At Base Two, it all starts with a human touch."
+      description="Software that moves people. At Base Two, it all starts with a human touch.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu augue non nisl feugiat porta. Fusce tempus molestie orci, ac lobortis mauris molestie vel. Nunc in tortor ut justo molestie rutrum. Nulla justo elit, mattis sed euismod a, viverra sed urna. Curabitur arcu ipsum, scelerisque eu lorem nec, ultrices sollicitudin sem. Cras ac sem quis dolor porta vehicula quis a mi. Aliquam erat volutpat. Cras pharetra turpis mollis urna lobortis, sed rhoncus sem finibus. Suspendisse quis cursus eros.
+
+Nam eget nisi vehicula, lacinia arcu at, fringilla lacus. Sed dictum non nisi vel euismod. Donec viverra ante nunc, laoreet tempor lacus."
       title="Base Two"
     >
       <HomeHero />
