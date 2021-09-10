952f9a3638954df1ee50ff634c5a3c07bd5c654b
Laura Padilla <hi@lauracodes.com>
2021-09-10 15:06:05 -0400

    WIP: Upadate blog seo description

--- src/components/BlogPage/index.tsx
+++ src/components/BlogPage/index.tsx
@@ -4,7 +4,10 @@ import { Layout } from '~/components';
 
 export const BlogPage: FC = () => {
   return (
-    <Layout description="Blog" title="Blog">
+    <Layout
+      description="Get tips and insights on current web trends, code best practices and more from the Base Two staff."
+      title="Blog"
+    >
       <h1>hello from the blog page</h1>
     </Layout>
   );
