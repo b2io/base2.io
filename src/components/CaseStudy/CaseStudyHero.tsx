0f6e74cd500c67cf35a4e7184a302dfd746ea08a
ewaight12 <ewaight12@gmail.com>
2021-09-15 10:47:11 -0400

    Progress on Case Study component

--- src/components/CaseStudy/CaseStudyHero.tsx
+++ src/components/CaseStudy/CaseStudyHero.tsx
@@ -1,5 +1,23 @@
+import { css } from '@emotion/react';
+import NextImage, { ImageProps as NextImageProps } from 'next/image';
 import type { FC } from 'react';
 
-export const CaseStudyHero: FC = () => {
-  return <h1>Hello from case study hero</h1>;
+type CaseStudyHeroProps = NextImageProps & {
+  alt: string;
+  src: Exclude<NextImageProps['src'], string | StaticImageData>;
+};
+
+export const CaseStudyHero: FC<CaseStudyHeroProps> = ({ src, alt }) => {
+  return (
+    <NextImage
+      alt={alt}
+      layout="responsive"
+      src={src}
+      css={css`
+        div {
+          z-index: 2;
+        }
+      `}
+    />
+  );
 };
--- src/components/NetJetsPage/index.tsx
+++ src/components/NetJetsPage/index.tsx
@@ -1,12 +1,18 @@
 import type { FC } from 'react';
 
-import { Layout } from '~/components';
+import { CaseStudy } from '~/components';
 
 export const NetJetsPage: FC = () => {
-  // Add description for NetJetsPage.
   return (
-    <Layout description="" title="NetJets">
-      <h1>hello from the netjets page</h1>
-    </Layout>
+    <CaseStudy
+      alt="NetJets"
+      client="Fly Netjets"
+      src={require('./images/netjets-hero.jpg')}
+      introText="With its wide range of highly personalized, upscale flight services,
+      NetJets offers luxury clients the moon. One indulgence was missing,
+      though: a sleek mobile app that lets them book flights, access
+      high-speed internet from midair, and even control cabin temperature at
+      the touch of a button."
+    />
   );
 };
