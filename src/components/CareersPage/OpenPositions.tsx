176a13bb6ef0ad072432cbc20f877cd6ecab6007
Laura Padilla <hi@lauracodes.com>
2021-09-20 11:08:14 -0400

    WIP: Remove extra React import at top

--- src/components/CareersPage/OpenPositions.tsx
+++ src/components/CareersPage/OpenPositions.tsx
@@ -1,7 +1,6 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import type { FC } from 'react';
-import React from 'react';
 
 import { Card, Heading, Text } from '~/components';
 import { atMinTablet, atMinXL, colors, spacing } from '~/theme';
