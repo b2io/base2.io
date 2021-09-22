f0d81b7e3b8058419890f7f5bbb9b2bfb0919c27
Laura Padilla <hi@lauracodes.com>
2021-09-22 15:36:20 -0400

    WIP: Replace position description

--- src/components/CareersPage/CareersData.tsx
+++ src/components/CareersPage/CareersData.tsx
@@ -28,27 +28,27 @@ export const benefitsData = [
 
 export const PositionsData = [
   {
-    desc: 'We believe that your job should make the "life" portion of your work-life balance a little more enjoyable and a little less stressful. This means you shouldn not have to retrofit your life to your work, rather, the other way around, which is why we offer a flexible work schedule and why our employees choose the Base Two location that best suits them.',
+    desc: 'We are always looking for amazing people to work with us in Columbus, OH; Pittsburgh, PA; and Chicago, IL. If you are a talented and creative individual with strong people skills and experience building web applications with the latest front-end technologies, please apply.',
     id: 1,
     job: 'Software Engineer',
   },
   {
-    desc: 'Health insurance is important, but nobody likes losing a portion of their paycheck to it. Base Two pays 100% of the premiums for employee health, vision, and dental insurance so that you dont need to worry about it.',
+    desc: 'We are always looking for amazing people to work with us in Columbus, OH; Pittsburgh, PA; and Chicago, IL. If you are a talented and creative individual with strong people skills and experience building web applications with the latest front-end technologies, please apply.',
     id: 2,
     job: 'Web developer',
   },
   {
-    desc: 'Saving for retirement is important, and Base Twos 401k plan provides a method to save with pre-tax money. To help with your savings Base Two contributes 3% of your salary to your Base Two 401k plan.',
+    desc: 'We are always looking for amazing people to work with us in Columbus, OH; Pittsburgh, PA; and Chicago, IL. If you are a talented and creative individual with strong people skills and experience building web applications with the latest front-end technologies, please apply.',
     id: 3,
     job: 'Senior software engineer',
   },
   {
-    desc: "Employees are encouraged to take an average of 3 weeks of vacation a year. We also close the office for 12 total holidays and offer unlimited sick days. Let's face it, we all get sick. ",
+    desc: 'We are always looking for amazing people to work with us in Columbus, OH; Pittsburgh, PA; and Chicago, IL. If you are a talented and creative individual with strong people skills and experience building web applications with the latest front-end technologies, please apply.',
     id: 4,
     job: 'Junior Developer',
   },
   {
-    desc: 'Base Two does not have standard-issue equipment to hand new employees, but rather allows each employee to select the equipment that will work best for them. If it makes you happy and helps you do your work, then it’s worth it.',
+    desc: 'We are always looking for amazing people to work with us in Columbus, OH; Pittsburgh, PA; and Chicago, IL. If you are a talented and creative individual with strong people skills and experience building web applications with the latest front-end technologies, please apply.',
     id: 5,
     job: 'Mid/Senior Software Engineer',
   },
