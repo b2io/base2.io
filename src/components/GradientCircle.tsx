2d61a42d11be6b5768786506013f4116c682d151
Laura Padilla <hi@lauracodes.com>
2021-10-15 13:14:08 -0400

    WIP: Add Drew's split circle animation

--- src/components/GradientCircle.tsx
+++ src/components/GradientCircle.tsx
@@ -1,28 +1,15 @@
 import { useId } from '@react-aria/utils';
-import {
-  animate,
-  motion,
-  useAnimation,
-  useMotionValue,
-  useTransform,
-} from 'framer-motion';
+import { animate, motion, useMotionValue, useTransform } from 'framer-motion';
 import { FC, useEffect, useRef } from 'react';
 import { useIntersection } from 'react-use';
 
 import { colors, interpolateColors, mRound } from '~/theme';
 
-const circleVariants = {
-  hidden: { opacity: 0, scale: 0 },
-  visible: {
-    opacity: 1,
-    scale: 1,
-    transition: { duration: 1, ease: 'easeInOut' },
-  },
-};
-
 export const GradientCircle: FC = (props) => {
   const filterId = useId();
   const gradientId = useId();
+  const clip1Id = useId();
+  const clip2Id = useId();
 
   const ref = useRef(null);
   const inView =
@@ -42,14 +29,6 @@ export const GradientCircle: FC = (props) => {
     }
   }, [inView, step]);
 
-  const controls = useAnimation();
-
-  useEffect(() => {
-    if (inView) {
-      controls.start('visible');
-    }
-  }, [controls, inView]);
-
   const fx = useTransform(step, (v) => {
     // Use a cosine wave to oscillate through [0, 0.5, 1, 0.5, 0] based on v.
     return mRound(0.5 * Math.cos((Math.PI / 2) * (v + 2)) + 0.5, 5);
@@ -60,20 +39,57 @@ export const GradientCircle: FC = (props) => {
     return mRound(0.5 * Math.cos((Math.PI / 2) * (v + 1)) + 0.5, 5);
   });
 
+  const step2 = useMotionValue(10);
+
+  useEffect(() => {
+    if (inView) {
+      const controls = animate(step2, 0, {
+        delay: 0.5,
+        duration: 1,
+        ease: 'easeOut',
+      });
+
+      return controls.stop;
+    }
+  }, [inView, step2]);
+
+  const cy1 = useTransform(step2, (v) => {
+    return mRound(50 - v, 5);
+  });
+
+  const cy2 = useTransform(step2, (v) => {
+    return mRound(50 + v, 5);
+  });
+
   return (
-    <motion.svg
-      animate={controls}
+    <svg
       height="100"
-      initial="hidden"
+      overflow="visible"
       ref={ref}
-      style={{ originX: '50%', originY: '50%' }}
-      variants={circleVariants}
       viewBox="0 0 100 100"
       width="100"
       {...props}
     >
       <defs>
-        <motion.radialGradient fx={fx} fy={fy} id={gradientId} r="100%">
+        <clipPath id={clip1Id}>
+          <motion.path
+            d="M0,50 A50,50 0 0,1 100,50 L0,50Z"
+            style={{ originX: '50%', originY: '50%', x: step2 }}
+            transformTemplate={({ x }) => {
+              return `rotate(90deg) translateX(-${x})`;
+            }}
+          />
+        </clipPath>
+        <clipPath id={clip2Id}>
+          <motion.path
+            d="M0,50 A50,50 0 0,1 100,50 L0,50Z"
+            style={{ originX: '50%', originY: '50%', x: step2 }}
+            transformTemplate={({ x }) => {
+              return `rotate(-90deg) translateX(-${x})`;
+            }}
+          />
+        </clipPath>
+        <motion.radialGradient fx={fx} fy={fy} r="100%" id={gradientId}>
           {interpolateColors([colors.coral, colors.darkBlueAlt], 1).map(
             (color, index, colors) => {
               const offset = index / (colors.length - 1);
@@ -98,14 +114,22 @@ export const GradientCircle: FC = (props) => {
           <feBlend in="SourceGraphic" in2="turbulence" mode="multiply" />
         </filter>
       </defs>
-      <circle
-        clipPath="circle(50%)"
+      <motion.circle
+        clipPath={`url('#${clip1Id}')`}
         cx="50"
-        cy="50"
+        cy={cy1}
         fill={`url('#${gradientId}')`}
         filter={`url('#${filterId}')`}
         r="50"
       />
-    </motion.svg>
+      <motion.circle
+        clipPath={`url('#${clip2Id}')`}
+        cx="50"
+        cy={cy2}
+        fill={`url('#${gradientId}')`}
+        filter={`url('#${filterId}')`}
+        r="50"
+      />
+    </svg>
   );
 };
