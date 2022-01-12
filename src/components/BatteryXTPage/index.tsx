ea0542bb9952c9ef555e0a58d6e633e288f0e154
EricWaight <ewaight12@gmail.com>
2022-01-11 19:20:48 -0500

    Content for BatteryXT case study

--- src/components/BatteryXTPage/index.tsx
+++ src/components/BatteryXTPage/index.tsx
@@ -1,8 +1,11 @@
 import type { FC } from 'react';
 
 import {
+  CaseStudyBottomNav,
   CaseStudyHeaderPane,
+  CaseStudyIntroContentPane,
   CaseStudyResultsPane,
+  CaseStudySummaryPane,
   Layout,
 } from '~/components';
 
@@ -31,6 +34,23 @@ export const BatteryXTPage: FC = () => {
       'By breaking down a complicated software into a bite-size application, we’ve provided DNV GL with a versatile marketing tool. Since its completion, the demo has helped the company generate leads, convert prospects into clients, and communicate the merit of BatteryXT to stakeholders across the world.',
   };
 
+  const caseStudyBottomNavProps = {
+    children: [
+      {
+        company: 'NetJets',
+        imagePath: '/work/net-jets.jpg',
+        navPath: '/work/netjets',
+        title: 'Fly Netjets',
+      },
+      {
+        company: 'AEP',
+        imagePath: '/work/scopebuilder.jpg',
+        navPath: '/work/scopebuilder',
+        title: 'ScopeBuilder',
+      },
+    ],
+  };
+
   return (
     <Layout description="" title="BatteryXT">
       <CaseStudyHeaderPane
@@ -38,7 +58,30 @@ export const BatteryXTPage: FC = () => {
         client="BatteryXT"
         src={require('./images/batteryxt-hero.jpg')}
       />
+      <CaseStudyIntroContentPane
+        introText="DNV GL, an international risk management group, helps people across the globe
+        realize new possibilities in battery power. Base Two simplified DNV GL’s BatteryXT software
+        into an accessible demo site that still captures the product’s core value."
+        askText="With BatteryXT, DNV GL’s clients can visualize battery lifespan and maintenance
+        needs across multiple applications and conditions. Built using highly technical MATLAB®
+        software, BatteryXT forms an imposing product. Our client wanted to contain it in an interactive
+        demo, so that professionals from any background could view it on any device—and quickly understand
+        its powerful benefits."
+        solutionText="Distilling software that stores extensive data into a small web application is no
+        easy feat. By collaborating with DNV GL’s marketing and engineering teams, Base Two could identify
+        the software’s most important functions—while also simplifying complex industry jargon and delivering
+        the desired demo version."
+      />
+      <CaseStudySummaryPane
+        summaryText="The new BatteryXT demo allows users to toggle between battery sizes, states of charge,
+        and temperatures within different applications—and see how these factors affect performance. It’s
+        compatible with nearly every device type and screen size, making it ideal for use on home computers
+        or trade show floors."
+        src={require('./images/batteryxt-summary.jpg')}
+        alt="screenshot"
+      />
       <CaseStudyResultsPane {...CaseStudyResultsResultsProps} />
+      <CaseStudyBottomNav {...caseStudyBottomNavProps} />
     </Layout>
   );
 };
--- src/components/NetJetsPage/index.tsx
+++ src/components/NetJetsPage/index.tsx
@@ -62,19 +62,19 @@ export const NetJetsPage: FC = () => {
       />
       <CaseStudyIntroContentPane
         introText="With its wide range of highly personalized, upscale flight services,
-      NetJets offers luxury clients the moon. One indulgence was missing,
-      though: a sleek mobile app that lets them book flights, access
-      high-speed internet from midair, and even control cabin temperature at
-      the touch of a button."
+        NetJets offers luxury clients the moon. One indulgence was missing,
+        though: a sleek mobile app that lets them book flights, access
+        high-speed internet from midair, and even control cabin temperature at
+        the touch of a button."
         askText="When NetJets’ call center became overloaded with booking traffic, they
-      realized a user-friendly app could reduce congestion and give customers more
-      control over their flying experience. To lift the idea off the ground, the private
-      jet charter company hired Base Two."
+        realized a user-friendly app could reduce congestion and give customers more
+        control over their flying experience. To lift the idea off the ground, the private
+        jet charter company hired Base Two."
         solutionText="Although NetJets had an existing mobile app, it was only compatible
-      with iOS and didn’t serve much functional purpose. In less time and with a smaller
-      budget than the original iOS build, our team created a new app that catered to both
-      Android and iPhone users. The result was Fly NetJets, a React Native app that makes
-      flight bookings easy and elegant."
+        with iOS and didn’t serve much functional purpose. In less time and with a smaller
+        budget than the original iOS build, our team created a new app that catered to both
+        Android and iPhone users. The result was Fly NetJets, a React Native app that makes
+        flight bookings easy and elegant."
       />
       <section
         css={css`
@@ -95,7 +95,7 @@ export const NetJetsPage: FC = () => {
         summaryText="Fly NetJets lets clients manage their accounts, book upcoming trips, and 
         fully control their in-flight experience. Behind the scenes, a number of systems communicate 
         to make this all possible, from a shared data layer supporting mobile and web interfaces to 
-        constantly changing updates on aircraft availability. "
+        constantly changing updates on aircraft availability."
         src={require('./images/NJ_OP.jpg')}
         alt="screenshot"
       />
