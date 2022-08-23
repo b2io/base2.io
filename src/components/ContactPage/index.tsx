96d0505d3b5707fc23e83e77718ecdebdf094702
Drew Miller <dmiller@base2.io>
2022-08-23 13:59:56 -0400

    update location

--- src/components/ContactPage/index.tsx
+++ src/components/ContactPage/index.tsx
@@ -34,9 +34,9 @@ export const ContactPage: FC = () => {
         variant="h2"
       >
         Our office is based in Columbus, OH. Our team members are located in
-        Pittsburgh, Philadelphia, Atlanta, New York City, and Orlando, but
-        thanks to satellites and the internet, we can work with you wherever you
-        are.
+        Columbus, Pittsburgh, Philadelphia, Atlanta, New York City, and Orlando,
+        but thanks to satellites and the internet, we can work with you wherever
+        you are.
       </Text>
       <Contact />
     </Layout>
