ce04166293428f7df340d055ff9e02a1b0363e5f
Kate Hansen <k8hansen@gmail.com>
2022-09-07 14:09:53 -0400

    WIP image "grid"

--- src/components/ColorSnapVisualizerPage/ColorSnapImagePanel.tsx
+++ src/components/ColorSnapVisualizerPage/ColorSnapImagePanel.tsx
@@ -1,20 +1,17 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import NextImage, { ImageProps as NextImageProps } from 'next/image';
 import { FC } from 'react';
 
-import { atMinLg, spacing } from '~/theme';
+import { atMinLg, atMinXL, atMinXXL, colors, cssClamp, spacing } from '~/theme';
 
 const ImageContainer = styled.div`
-  top: -18rem;
+  display: inline-block;
   height: 100%;
-  justify-content: center;
-  position: relative;
+  margin: -18em 1em 0 1em;
 
   ${atMinLg} {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    --column-shift: ${spacing.xxl3};
-    gap: ${spacing.xxl};
+    margin-top: -18em;
   }
 `;
 
@@ -25,7 +22,52 @@ type ImagePanelItemProps = {
 
 const ImagePanelItem: FC<ImagePanelItemProps> = ({ src, alt }) => {
   return (
-    <div>
+    <div
+      css={css`
+        padding: 1rem 0;
+        display: inline-block;
+        position: relative;
+
+        ${atMinLg} {
+          padding: 3rem 0;
+          width: 85%;
+
+          &:nth-of-type(odd) {
+            right: calc(40% - 50vw) !important;
+          }
+
+          &:nth-of-type(even) {
+            right: calc(55% - 50vw) !important;
+          }
+        }
+
+        ${atMinXL} {
+          padding: 3rem 0;
+          width: 85%;
+
+          &:nth-of-type(odd) {
+            right: calc(42% - 50vw) !important;
+          }
+
+          &:nth-of-type(even) {
+            right: calc(55% - 50vw) !important;
+          }
+        }
+
+        ${atMinXXL} {
+          padding: 3rem 0;
+          width: 85%;
+
+          &:nth-of-type(odd) {
+            right: calc(42% - 50vw) !important;
+          }
+
+          &:nth-of-type(even) {
+            right: calc(68% - 50vw) !important;
+          }
+        }
+      `}
+    >
       <NextImage src={src} alt={alt} />
     </div>
   );
@@ -42,23 +84,11 @@ export const ColorSnapImagePanel: FC<ColorSnapImagePanelProps> = (props) => {
       <ImageContainer>
         <ImagePanelItem
           alt="image"
-          src={require('./images/colorsnap-samples/cs1_sample.png')}
-        />
-        <ImagePanelItem
-          alt="image"
           src={require('./images/colorsnap-samples/cs2_sample.png')}
         />
         <ImagePanelItem
           alt="image"
           src={require('./images/colorsnap-samples/cs3_sample.png')}
-        />
-        <ImagePanelItem
-          alt="image"
-          src={require('./images/colorsnap-samples/cs4_sample.png')}
-        />
-        <ImagePanelItem
-          alt="image"
-          src={require('./images/colorsnap-samples/cs5_sample.png')}
         />
         <ImagePanelItem
           alt="image"
