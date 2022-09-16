85fe6d30bdc0f3edccd20ec2309014865b15b939
Kate Hansen <k8hansen@gmail.com>
2022-09-16 14:41:07 -0400

    WIP: width on larger sizes

--- src/components/ColorSnapVisualizerPage/ColorSnapImagePanel.tsx
+++ src/components/ColorSnapVisualizerPage/ColorSnapImagePanel.tsx
@@ -3,24 +3,13 @@ import styled from '@emotion/styled';
 import NextImage, { ImageProps as NextImageProps } from 'next/image';
 import { FC } from 'react';
 
-import {
-  atMinDesktop,
-  atMinLg,
-  atMinTablet,
-  atMinXL,
-  atMinXXL,
-  cssClamp,
-} from '~/theme';
+import { atMinLg, atMinXL } from '~/theme';
 
 const ImageContainer = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
   margin: -18em 1em 0 1em;
-
-  /* ${atMinTablet} {
-    width: ${cssClamp([34.313, 'tablet'], [64.5, 'desktop'])};
-  } */
 
   ${atMinLg} {
     margin-top: -18em;
@@ -43,58 +32,19 @@ const ImagePanelItem: FC<ImagePanelItemProps> = ({ src, alt }) => {
         display: flex;
         justify-content: center;
 
-        ${atMinLg} {
-          padding: 3rem 0;
-          &:nth-of-type(odd) {
-            align-self: end;
-            width: 50%;
-          }
-
-          &:nth-of-type(even) {
-            align-self: start;
-            width: 50%;
-          }
-        }
-
         ${atMinXL} {
           padding: 3rem 0;
 
           &:nth-of-type(odd) {
             align-self: end;
-            width: 50%;
+            width: 75%;
           }
 
           &:nth-of-type(even) {
             align-self: start;
-            width: 50%;
+            width: 75%;
           }
         }
-        /* 
-        ${atMinXXL} {
-          padding: 3rem 0;
-          width: 85%;
-
-          &:nth-of-type(odd) {
-            right: calc(42% - 50vw) !important;
-          }
-
-          &:nth-of-type(even) {
-            left: calc(70% - 50vw) !important;
-          }
-        }
-
-        ${atMinDesktop} {
-          padding: 3rem 0;
-          width: 85%;
-
-          &:nth-of-type(odd) {
-            right: calc(42% - 50vw) !important;
-          }
-
-          &:nth-of-type(even) {
-            left: calc(115% - 50vw) !important;
-          }
-        } */
       `}
     >
       <NextImage src={src} alt={alt} />
