6cf5a56312dc6c22ebcce38464286f90d8a60bc1
Kate Hansen <k8hansen@gmail.com>
2023-04-17 09:49:30 -0400

    wip

---
+++ src/components/B2Logo.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+
+function B2Logo() {
+  return (
+    <Image
+      src="../../public/logo-primary.png"
+      alt="base two"
+      width="64"
+      height="64"
+    />
+  );
+}
+
+export default B2Logo;
