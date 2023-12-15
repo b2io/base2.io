8e507c5ba3757bffa66d9568b1fe26867f92ed13
Nelson Foltz <nfoltz@base2.io>
2023-12-15 13:53:42 -0500

    CaseStudyHero

--- src/components/CaseStudy/CaseStudyHero.tsx
+++ src/components/CaseStudy/CaseStudyHero.tsx
@@ -2,7 +2,7 @@ import { css } from '@emotion/react';
 import NextImage, {
   ImageProps as NextImageProps,
   StaticImageData,
-} from "next/legacy/image";
+} from 'next/image';
 import type { FC } from 'react';
 
 type CaseStudyHeroProps = NextImageProps & {
@@ -14,13 +14,11 @@ export const CaseStudyHero: FC<CaseStudyHeroProps> = ({ src, alt }) => {
   return (
     <NextImage
       alt={alt}
-      layout="responsive"
       src={src}
       css={css`
         filter: opacity(65%);
-        div {
-          z-index: 2;
-        }
+        height: auto;
+        width: 100%;
       `}
     />
   );
