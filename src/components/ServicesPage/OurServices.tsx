a6c5618a11b71ee426b4f464dacb970ee7141bfa
laurapadilla <hi@laurapadilla.xyz>
2024-06-07 13:49:06 -0400

    Use Element tag

--- src/components/ServicesPage/OurServices.tsx
+++ src/components/ServicesPage/OurServices.tsx
@@ -1,7 +1,7 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import type { FC } from 'react';
-import { Link } from 'react-scroll';
+import { Element, Link } from 'react-scroll';
 
 import { Heading, SectionContainer, ServiceCard, Text } from '~/components';
 import { atMinLg, atMinTablet, colors, cssClamp, spacing } from '~/theme';
@@ -53,12 +53,12 @@ const ServicesContainer = styled.section`
   margin-top: ${spacing.xxl2};
 `;
 
-const Service = styled.div`
-  display: flex;
-  flex: 1 1 auto;
-  flex-wrap: wrap;
-  gap: ${spacing.xs};
-`;
+// const Service = styled.div`
+//   display: flex;
+//   flex: 1 1 auto;
+//   flex-wrap: wrap;
+//   gap: ${spacing.xs};
+// `;
 
 const ServiceCardContainer = styled.div`
   display: flex;
@@ -152,13 +152,18 @@ export const OurServices: FC = () => {
           const multipleServices = service.services.length > 1;
 
           return (
-            <Service
-              key={`service-${index + 1}`}
+            <Element
               css={css`
+                display: flex;
+                flex: 1 1 auto;
+                flex-wrap: wrap;
+                gap: ${spacing.xs};
                 ${atMinTablet} {
                   max-width: ${!multipleServices && 'calc(50% - 1.75rem)'};
                 }
               `}
+              key={`service-${index + 1}`}
+              name={service.sectionTitle}
             >
               {multipleServices && (
                 <>
@@ -205,7 +210,7 @@ export const OurServices: FC = () => {
                   />
                 ))}
               </ServiceCardContainer>
-            </Service>
+            </Element>
           );
         })}
       </ServicesContainer>
