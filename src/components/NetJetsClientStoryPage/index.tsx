bb1ac9b7167e9ce6924a26967ad700b68792f0f2
Kate Hansen <k8hansen@gmail.com>
2024-06-26 12:33:10 -0400

    WIP

---
+++ src/components/NetJetsClientStoryPage/index.tsx
@@ -0,0 +1,8 @@
+import { Layout } from '../Layouts/Layout';
+import type { FC } from 'react';
+
+export const NetJetsClientStoryPage: FC = () => {
+  return (
+    <Layout description="Case study about NetJets" title="Netjets"></Layout>
+  );
+};
